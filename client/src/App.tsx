f16d89692124f17e21c6324d5cf7c295d9d3fc6e
ilangoldman <ilangoldman@macbook.local>
2019-09-08 18:58:09 -0300

    init react on rails

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/assets/react_rails.png" alt="logo" />
+        <br />
+        <p>Welcome to React on Rails!</p>
+        <p>
+          This will be home of all the mighty knowledge of these two beasts
+          combined.
+        </p>
+        <p>To be continued...</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
